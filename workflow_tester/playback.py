c3324f52dbe53898ff63b91ad9e67b02021f5187
danny crasto <danwald79@gmail.com>
2023-05-14 20:10:58 +0400

    Can now advance the player for an interaction

--- workflow_tester/playback.py
+++ workflow_tester/playback.py
@@ -1,10 +1,11 @@
 from collections import defaultdict
 import typing
 import dataclasses
-import content
 import logging
+import constants  # noqa F401
 
 logger = logging.getLogger(__name__)
+
 
 @dataclasses.dataclass
 class Cursor:
@@ -15,18 +16,39 @@ class Cursor:
 class Player:
     def __init__(self, filename: str):
         self.tape = self._load_tape(filename)
-        self.head = {}
+        self.head = defaultdict(int)
 
-    def _load_tape(filename) -> typing.Dict(str, typing.List(Cursor)):
+    @staticmethod
+    def interaction_key(event, user) -> str:
+        return f'{event}:{user}'
+
+    def get_n_advance_interaction(self, event, user) -> typing.Optional[Cursor]:
+        interaction = self.interaction_key(event, user)
+        pos = self.head[interaction]
+        self.head[interaction] += 1
+        try:
+            return self.tape[interaction][pos]
+        except IndexError:
+            return None
+
+    def _load_tape(self, filename) -> defaultdict[str, typing.List[Cursor]]:
         spool = defaultdict(list)
         with open(filename) as fp:
             for line in fp:
                 record = line.split(',')
+                logger.debug('Processing: "%s"', record)
                 if record[0].strip() == '#':
                     continue
-                event_code, user_phone, *interaction = *record
+                event_code, user_phone, *interaction = record
+                if len(interaction) % 2:
+                    logger.error(
+                        'Invalid script. Not paired sequenece (%s:%s %s)',
+                        event_code, user_phone, interaction
+                    )
+                    continue
                 cursor = iter(interaction)
                 for pos in cursor:
-                    spool['f{event_code:user_phone}'].append(
-                        Cursor(pos, next(cursor))
+                    spool[self.interaction_key(event_code, user_phone)].append(
+                        Cursor(pos.strip(), (next(cursor)).strip())
                     )
+        return spool
